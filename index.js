35281c9bff040e52e8961a9815f70a53cb15df3b
TrustedDev1325 <olamideliam@gmail.com>
2021-10-06 02:31:28 +0900

    Get the data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function scraperStart(){
-
+    await scraperPage();
 }
 
 var scraperPage = async function(){
@@ -12,5 +12,16 @@ var scraperPage = async function(){
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto(url);
+    await page.waitForSelector(".api-module--apiData--15-o7");
+    let infos = await page.evaluate(() => {
+        let infos = document.body.querySelector(".api-module--apiData--15-o7").textContent;
+        return infos;
+    })
+    console.log(infos);
+    // for (var i = 0 ; i<infos.length; i++){
+    //     console.log(infos[i])
+    // }
+    page.close();
+    browser.close();
 }
 scraperStart();
